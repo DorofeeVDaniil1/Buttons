ac19e071d274555a64453b03429b0e19ba33043d
DorofeeVDaniil1 <82593323+DorofeeVDaniil1@users.noreply.github.com>
2021-06-25 11:34:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/canvas/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.canvas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Canvas;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.myscrreen);
+        
+    }
+
+}
